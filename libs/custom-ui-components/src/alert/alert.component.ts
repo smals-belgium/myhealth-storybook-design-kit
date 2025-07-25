b6ad434cf21cfcb69ad4e53cd3bac93fd9b208ff
smals-lule <lucas.lebrun@smals.be>
2025-07-25 09:59:25 +0200

    chore(alert): make alertTitle non-required (#30)
    * chore(alert): make alertTitle non-required
    
    * chore(alert): update doc

--- libs/custom-ui-components/src/alert/alert.component.ts
+++ libs/custom-ui-components/src/alert/alert.component.ts
@@ -12,7 +12,7 @@ import { MatButton } from '@angular/material/button';
 export class AlertComponent {
   usage = input<'info' | 'success' | 'warning' | 'error' | 'notification'>('info');
   backgroundColor = input<'white' | 'color'>('color');
-  alertTitle = input.required<string>();
+  alertTitle = input<string>();
   details = input<string>();
   actionButton = input<boolean>(false);
   buttonDisabled = input<boolean>(false);
