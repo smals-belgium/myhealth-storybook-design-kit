6cd8a27ebf45b5824db13fc1d1b5ea1e7382661d
mini <mieke.nijs@smals.be>
2026-03-31 14:40:21 +0200

    feat(button-toggle): add error state to button-toggle

--- libs/storybook/src/design-kit/button-toggle/button-toggle.component.ts
+++ libs/storybook/src/design-kit/button-toggle/button-toggle.component.ts
@@ -1,12 +1,42 @@
-import { Component, input, InputSignal } from '@angular/core';
+import { Component, effect, input, InputSignal } from '@angular/core';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatError, MatFormField, MatLabel, MatSuffix } from '@angular/material/input';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'mh-button-toggle',
-  imports: [MatButtonToggleModule],
+  imports: [
+    MatButtonToggleModule,
+    ReactiveFormsModule,
+    MatError,
+    MatIcon,
+    MatSuffix,
+    MatFormField,
+    MatLabel,
+    MatButton,
+  ],
   templateUrl: './button-toggle.component.html',
   styleUrl: './button-toggle.component.scss',
 })
 export class ButtonToggleComponent {
+  readonly view = input<'default' | 'error'>('default');
   readonly disabled: InputSignal<boolean> = input.required();
+  readonly fontStyleControl = new FormControl(
+    { value: undefined, disabled: false },
+    { validators: [Validators.required] }
+  );
+
+  constructor() {
+    this.fontStyleControl.markAsTouched();
+
+    effect(() => {
+      if (this.disabled()) {
+        this.fontStyleControl.disable();
+      } else {
+        this.fontStyleControl.enable();
+      }
+    });
+  }
 }
